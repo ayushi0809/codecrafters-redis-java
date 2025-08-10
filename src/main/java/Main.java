d792557be376e4bb557d28a3216b0c43b24d8cd3
Ayushi <khandeliaayushi08@gmail.com>
2025-08-10 17:06:43 +0530

    stage 4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.io.OutputStream;
 import java.net.Socket;
@@ -30,18 +32,25 @@ public class Main {
   }
 
   static void handleClient(Socket clientSocket) {
-    try (OutputStream out = clientSocket.getOutputStream()) {
+    try (clientSocket; // automatically closes socket at the end
+        OutputStream outputStream = clientSocket.getOutputStream();
+        BufferedReader in = new BufferedReader(
+            new InputStreamReader(clientSocket.getInputStream()))) {
       // Send a simple response to the client
-      out.write("+PONG\\r\\n".getBytes());
-      out.flush();
+      while (true) {
+        // hacky way to read input; will change
+        if (in.readLine() == null) {
+          break;
+        }
+        in.readLine();
+        String line = in.readLine();
+        System.out.println("Last line: " + line);
+
+        outputStream.write("+PONG\r\n".getBytes());
+        System.out.println("Wrote pong");
+      }
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
-    } finally {
-      try {
-        clientSocket.close();
-      } catch (IOException e) {
-        System.out.println("IOException: " + e.getMessage());
-      }
     }
   }
 }
